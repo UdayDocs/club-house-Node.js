cdb131b87d84e1ee8befa81b33f1bec582dcd1a7
UdayDocs <137787964+UdayDocs@users.noreply.github.com>
2025-03-20 14:04:21 +0530

    f commit

--- models/message.js
+++ models/message.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../app').sequelize;
+const sequelize = require('../db');
 const User = require('./user');
 
 const Message = sequelize.define('Message', {
